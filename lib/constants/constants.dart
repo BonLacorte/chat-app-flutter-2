e37bc198a2166ac954d6a6bd256aa662b0ca5d02
Bon Lacorte <bonbonlacorte123@gmail.com>
2022-11-16 16:02:46 +0800

    commit-1

---
+++ lib/constants/constants.dart
@@ -0,0 +1,6 @@
+class Constants {
+  static String appId = "1:419011231740:web:2c2f608cc363214c9cbc79";
+  static String apiKey = "AIzaSyDMzSp28QMGjinaqt2TqkqbW2LQni8Qym4";
+  static String messagingSenderId = "419011231740";
+  static String projectId = "chart-app-flutter-2";
+}
